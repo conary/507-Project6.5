880a3956cc8907913c14cdc6190445696560a08d
conary <stefande@umich.edu>
2017-11-29 13:47:56 -0500

    Code working

--- sample_flask_app.py
+++ sample_flask_app.py
@@ -1,7 +1,8 @@
 # Import statements necessary
 from flask import Flask, render_template
 from flask_script import Manager
-
+import json
+import requests
 # Set up application
 app = Flask(__name__)
 
@@ -30,14 +31,28 @@ def basic_values_list(name):
 
 
 ## PART 1: Add another route /word/<new_word> as the instructions describe.
+@app.route('/word/<new_word>')
+def nursery_rhymes(new_word):
+
+    baseurl = "https://api.datamuse.com/words"
+
+    final_url = baseurl + '?' + 'rel_rhy=' + new_word
+    
 
 
+    result = json.loads(requests.get(final_url).text)
+
+    final_word = result[0]['word']
+
+
+
+    return '<h1>{}</h1>'.format(final_word)
+
+    
 ## PART 2: Edit the following route so that the photo_tags.html template will render
 @app.route('/flickrphotos/<tag>/<num>')
 def photo_titles(tag, num):
-    # HINT: Trying out the flickr accessing code in another file and seeing what data you get will help debug what you need to add and send to the template!
-    # HINT 2: This is almost all the same kind of nested data investigation you've done before!
-    FLICKR_KEY = "" # TODO: fill in a flickr key
+    FLICKR_KEY = "0efc6276f91dba2f5e77fc74755d38ff" 
     baseurl = 'https://api.flickr.com/services/rest/'
     params = {}
     params['api_key'] = FLICKR_KEY
@@ -49,9 +64,11 @@ def photo_titles(tag, num):
     response_obj = requests.get(baseurl, params=params)
     trimmed_text = response_obj.text[14:-1]
     flickr_data = json.loads(trimmed_text)
-    # TODO: Add some code here that processes flickr_data in some way to get what you nested
-    # TODO: Edit the invocation to render_template to send the data you need
-    return render_template('photo_tags.html')
+    photos = flickr_data['photos']['photo']
+    titles = []
+    for photo in photos:
+        titles.append(photo['title'])
+    return render_template('photo_info.html', photo_titles=titles, num=num)
 
 
 
